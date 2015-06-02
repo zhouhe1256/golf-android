6d0954125347d716f078b271f2056ab0c75635c5
zhaoxt <zhaoxt@bjcathay.com>
2015-06-02 18:44:37 +0800

    commit

--- app/src/main/java/com/bjcathay/qt/model/PushModel.java
+++ app/src/main/java/com/bjcathay/qt/model/PushModel.java
@@ -4,8 +4,9 @@ package com.bjcathay.qt.model;
  * Created by bjcathay on 15-5-22.
  */
 public class PushModel {
-   private String type;
-   private String target;
+    private String type;
+    private String target;
+    private String message;
 
     public String getType() {
         return type;
@@ -21,5 +22,13 @@ public class PushModel {
 
     public void setTarget(String target) {
         this.target = target;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
--- app/src/main/java/com/bjcathay/qt/receiver/MessageReceiver.java
+++ app/src/main/java/com/bjcathay/qt/receiver/MessageReceiver.java
@@ -1,40 +1,36 @@
 package com.bjcathay.qt.receiver;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.bjcathay.android.async.Arguments;
-import com.bjcathay.android.async.ICallback;
 import com.bjcathay.android.json.JSONUtil;
+import com.bjcathay.qt.R;
 import com.bjcathay.qt.activity.CompetitionDetailActivity;
-import com.bjcathay.qt.activity.ExerciseActivity;
 import com.bjcathay.qt.activity.OrderDetailActivity;
-import com.bjcathay.qt.application.GApplication;
-import com.bjcathay.qt.constant.ErrorCode;
 import com.bjcathay.qt.model.PushModel;
-import com.bjcathay.qt.model.UserModel;
-import com.bjcathay.qt.util.DialogUtil;
 import com.bjcathay.qt.util.PreferencesConstant;
 import com.bjcathay.qt.util.PreferencesUtils;
-import com.bjcathay.qt.util.ViewUtil;
 import com.igexin.sdk.PushConsts;
 import com.igexin.sdk.PushManager;
-
-import org.json.JSONObject;
 
 /**
  * Created by bjcathay on 15-5-22.
  */
 public class MessageReceiver extends BroadcastReceiver {
+
+    private static int ids = 1;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Bundle bundle = intent.getExtras();
         Log.d("GetuiSdkDemo", "onReceive() action=" + bundle.getInt("action"));
         switch (bundle.getInt(PushConsts.CMD_ACTION)) {
-
             case PushConsts.GET_MSG_DATA:
                 // 获取透传数据
                 // String appid = bundle.getString("appid");
@@ -60,6 +56,7 @@ public class MessageReceiver extends BroadcastReceiver {
                 // 获取ClientID(CID)
                 // 第三方应用需要将CID上传到第三方服务器，并且将当前用户帐号和CID进行关联，以便日后通过用户帐号查找CID进行消息推送
                 String cid = bundle.getString("clientid");
+//                PreferencesUtils.putString(GApplication.getInstance(), PreferencesConstant.PUSH_CLIENT_ID, cid);
                 break;
             case PushConsts.THIRDPART_FEEDBACK:
                 break;
@@ -70,21 +67,29 @@ public class MessageReceiver extends BroadcastReceiver {
 
     private void handlePush(Context context, PushModel pushModel) {
         PreferencesUtils.putBoolean(context, PreferencesConstant.NEW_MESSAGE_FLAG, true);
-        //ORDER|COMPETITION,
-        String type = pushModel.getType();
-        String target = pushModel.getTarget();
-        // Long columnId = pushProduct.getColumnId();
-        if ("ORDER".equals(type)) {
-            Intent intent = new Intent(context, OrderDetailActivity.class);
-            intent.putExtra("id", Long.valueOf(target));
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(context);
+        Intent intent = null;
+        if ("ORDER".equals(pushModel.getType())) {
+            intent = new Intent(context, OrderDetailActivity.class);
+            intent.putExtra("tid", Long.parseLong(pushModel.getTarget()));
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            ViewUtil.startActivity(context, intent);
-        } else if ("COMPETITION".equals(type)) {
-            Intent intent = new Intent(context, CompetitionDetailActivity.class);
-            intent.putExtra("url", target);
+        } else if ("COMPETITION".equals(pushModel.getType())) {
+            intent = new Intent(context, CompetitionDetailActivity.class);
+            intent.putExtra("url", pushModel.getTarget());
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            ViewUtil.startActivity(context, intent);
         }
 
+        PendingIntent contentIntent = PendingIntent.getActivity(context, ids, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+        Notification notification = builder.setTicker(pushModel.getMessage()).
+                setContentTitle("7铁高尔夫").
+                setContentText(pushModel.getMessage()).
+                setSmallIcon(R.drawable.ic_launcher).
+                build();
+
+        notification.defaults = Notification.DEFAULT_SOUND;
+        notification.flags = 0x00000010;
+        mNotificationManager.notify(ids++, notification);
     }
 }
